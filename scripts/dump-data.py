71b43e4342f19d64d0cd5348e46bb2862665f393
kshefchek <kshefchek@gmail.com>
2020-01-14 10:27:13 -0800

    add frequency and onset qualifiers to d2p

--- scripts/dump-data.py
+++ scripts/dump-data.py
@@ -26,10 +26,16 @@ parser.add_argument('--config', '-c', required=True, help='yaml configuration fi
 parser.add_argument('--out', '-o', required=False, help='output directory', default="./")
 parser.add_argument('--solr', '-s', required=False,
                     default="https://solr-dev.monarchinitiative.org/solr/golr/select",
-                    help='output directory')
+                    help='solr url')
 
 args = parser.parse_args()
 dir_path = Path(args.out)
+
+default_fields = '''
+    subject,subject_label,subject_taxon,subject_taxon_label,
+    object,object_label,relation,relation_label,evidence,evidence_label,
+    source,is_defined_by,qualifier
+'''
 
 golr_params = {
     'q': '*:*',
@@ -38,9 +44,6 @@ golr_params = {
     'csv.separator': '\t',
     'csv.header': 'true',
     'csv.mv.separator': '|',
-    'fl': 'subject,subject_label,subject_taxon,subject_taxon_label,'
-          'object,object_label,relation,relation_label,evidence,evidence_label,'
-          'source,is_defined_by,qualifier'
 }
 
 conf_fh = open(args.config, 'r')
@@ -54,11 +57,17 @@ for directory, file_maps in file_filter_map.items():
         dump_file = dump_dir / file
         dump_file_fh = dump_file.open("w")
 
+        golr_params['fl'] = default_fields
+
+        for filter in filters:
+            if filter == 'association_type:disease_phenotype':
+                golr_params['fl'] += ',onset,onset_label,frequency,frequency_label'
+
         count_params = {
             'wt': 'json',
             'rows': 0,
             'q': '*:*',
-            'fq': filters
+            'fq': filters,
         }
         solr_request = requests.get(args.solr, params=count_params)
         response = solr_request.json()
