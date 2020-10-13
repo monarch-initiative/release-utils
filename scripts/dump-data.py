824c8fd043bdc9856f27e4c66732778a37de68a7
kshefchek <kshefchek@gmail.com>
2020-10-13 15:50:31 -0400

    dump all solr associations as tsvs

--- scripts/dump-data.py
+++ scripts/dump-data.py
@@ -16,82 +16,120 @@ import argparse
 from pathlib import Path
 import logging
 import yaml
+import gzip
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-parser = argparse.ArgumentParser(usage=__doc__)
-parser.add_argument('--config', '-c', required=True, help='yaml configuration file')
-parser.add_argument('--out', '-o', required=False, help='output directory', default="./")
-parser.add_argument('--solr', '-s', required=False,
-                    default="https://solr-dev.monarchinitiative.org/solr/golr/select",
-                    help='solr url')
+def main():
 
-args = parser.parse_args()
-dir_path = Path(args.out)
+    parser = argparse.ArgumentParser(usage=__doc__)
+    parser.add_argument('--config', '-c', required=True, help='yaml configuration file')
+    parser.add_argument('--out', '-o', required=False, help='output directory', default="./")
+    parser.add_argument('--solr', '-s', required=False,
+                        default="https://solr-dev.monarchinitiative.org/solr/golr/select",
+                        help='solr url')
 
-default_fields = '''
-    subject,subject_label,subject_taxon,subject_taxon_label,
-    object,object_label,relation,relation_label,evidence,evidence_label,
-    source,is_defined_by,qualifier
-'''
+    args = parser.parse_args()
+    dir_path = Path(args.out)
 
-golr_params = {
-    'q': '*:*',
-    'wt': 'csv',
-    'csv.encapsulator': '"',
-    'csv.separator': '\t',
-    'csv.header': 'true',
-    'csv.mv.separator': '|',
-}
+    # Fetch all associations
 
-conf_fh = open(args.config, 'r')
-file_filter_map = yaml.safe_load(conf_fh)
-
-for directory, file_maps in file_filter_map.items():
-    dump_dir = dir_path / directory
+    association_dir = 'all_associations'
+    dump_dir = dir_path / association_dir
     dump_dir.mkdir(parents=True, exist_ok=True)
 
-    for file, filters in file_maps.items():
-        dump_file = dump_dir / file
-        dump_file_fh = dump_file.open("w")
+    # wt=json&facet=true&json.nl=arrarr&rows=0&q=*:*&facet.field=association_type
+    assoc_params = {
+        'q': '*:*',
+        'wt': 'json',
+        'json.nl': 'arrarr',
+        'rows': 0,
+        'facet': 'true',
+        'facet.field': 'association_type'
+    }
 
-        golr_params['fl'] = default_fields
+    solr_request = requests.get(args.solr, params=assoc_params)
+    response = solr_request.json()
 
-        for filter in filters:
-            if filter == 'association_type:disease_phenotype':
-                golr_params['fl'] += ',onset,onset_label,frequency,frequency_label'
+    for facet in response['facet_counts']['facet_fields']['association_type']:
+        association = facet[0]
+        file = "{}.all.tsv.gz".format(association)
+        dump_file_fh = gzip.open(dump_dir / file, 'wt')
+        filters = ['association_type:{}'.format(association)]
+        generate_tsv(dump_file_fh, args.solr, filters)
 
-        count_params = {
-            'wt': 'json',
-            'rows': 0,
-            'q': '*:*',
-            'fq': filters,
-        }
-        solr_request = requests.get(args.solr, params=count_params)
-        response = solr_request.json()
-        resultCount = response['response']['numFound']
+    # Fetch associations configured in args.config
+    conf_fh = open(args.config, 'r')
+    file_filter_map = yaml.safe_load(conf_fh)
 
-        if resultCount == 0:
-            logger.warn("No results found for {}"
-                        " with filters {}".format(file, filters))
+    for directory, file_maps in file_filter_map.items():
+        dump_dir = dir_path / directory
+        dump_dir.mkdir(parents=True, exist_ok=True)
 
-        golr_params['rows'] = 1000
-        golr_params['start'] = 0
-        golr_params['fq'] = filters
+        for file, filters in file_maps.items():
+            dump_file = dump_dir / file
+            dump_file_fh = dump_file.open("w")
 
-        first_res = True
+            generate_tsv(dump_file_fh, args.solr, filters)
 
-        while golr_params['start'] < resultCount:
-            if first_res:
-                golr_params['csv.header'] = 'true'
-                first_res = False
-            else:
-                golr_params['csv.header'] = 'false'
-            solr_request = requests.get(args.solr, params=golr_params)
-            solr_response = solr_request.text
-            dump_file_fh.write(solr_response)
-            golr_params['start'] += golr_params['rows']
+            dump_file_fh.close()
 
-        dump_file_fh.close()
+
+def generate_tsv(tsv_fh, solr, filters):
+    default_fields = '''
+        subject,subject_label,subject_taxon,subject_taxon_label,
+        object,object_label,relation,relation_label,evidence,evidence_label,
+        source,is_defined_by,qualifier
+    '''
+
+    golr_params = {
+        'q': '*:*',
+        'wt': 'csv',
+        'csv.encapsulator': '"',
+        'csv.separator': '\t',
+        'csv.header': 'true',
+        'csv.mv.separator': '|',
+        'fl': default_fields
+    }
+
+    for filter in filters:
+        if filter == 'association_type:disease_phenotype':
+            golr_params['fl'] += ',onset,onset_label,frequency,frequency_label'
+
+    count_params = {
+        'wt': 'json',
+        'rows': 0,
+        'q': '*:*',
+        'fq': filters,
+    }
+    solr_request = requests.get(solr, params=count_params)
+    response = solr_request.json()
+    resultCount = response['response']['numFound']
+
+    if resultCount == 0:
+        logger.warn("No results found for {}"
+                    " with filters {}".format(tsv_fh.name, filters))
+
+    golr_params['rows'] = 1000
+    golr_params['start'] = 0
+    golr_params['fq'] = filters
+
+    first_res = True
+
+    while golr_params['start'] < resultCount:
+        if first_res:
+            golr_params['csv.header'] = 'true'
+            first_res = False
+        else:
+            golr_params['csv.header'] = 'false'
+        solr_request = requests.get(solr, params=golr_params)
+        solr_response = solr_request.text
+        tsv_fh.write(solr_response)
+        golr_params['start'] += golr_params['rows']
+
+
+if __name__ == "__main__":
+    main()
+
